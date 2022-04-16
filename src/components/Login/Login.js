53c9cc2ab12f5d24cee940b287754a33b8cb10aa
razuahmedjoy <rjjoybzs420@gmail.com>
2022-04-17 01:54:48 +0600

    email password create account and login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../Partials/Loading/Loading';
 import SocialLogin from '../Partials/SocialLogin/SocialLogin';
 import './Login.css'
 const Login = () => {
+    const navigate = useNavigate()
     
     const [user, loading, error] = useAuthState(auth);
-    const navigate = useNavigate()
+    const [
+        signInWithEmailAndPassword,
+        user2,
+        loading2,
+        error2,
+      ] = useSignInWithEmailAndPassword(auth);
 
     // get previous page url
     const location = useLocation()
@@ -19,23 +25,35 @@ const Login = () => {
     if(loading) {
         return <Loading></Loading>
     }
-    if(user){
+    if(user || user2){
         navigate(from,{replace:true});
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value
+        const password = e.target.password.value
+        signInWithEmailAndPassword(email, password)
+
     }
     return (
         <div>
             <div className="form-container mx-2 mx-md-auto">
                 <h2 className="text-main text-center mb-4">Login Now</h2>
-                <Form>
+                <p><small className="text-danger">
+                    {error ? error:''}
+                    {error2 ? error2?.message:''}
+                    </small></p>
+                <Form onSubmit={handleFormSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control name="email" type="email" placeholder="Enter email" required />
                     
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                      
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control name="password" type="password" placeholder="Password" required />
                     </Form.Group>
                     <p>Don't have any account? <Link to="/register">Create Account Here</Link> </p>
                     <Button className="btn btn-outline-main bg-main text-light w-100" type="submit">
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,42 +1,83 @@
 import { Button } from 'react-bootstrap';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import SocialLogin from '../Partials/SocialLogin/SocialLogin';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../Partials/Loading/Loading';
 
 const Register = () => {
+
+    const emailRef = useRef()
+    const nameRef = useRef();
+    const pass1Ref = useRef();
+    const pass2Ref = useRef();
+
     const [user, loading, error] = useAuthState(auth);
+    const [
+        createUserWithEmailAndPassword,
+        createduser,
+        loading2,
+        error2,
+      ] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
+    const [updateProfile, updating, error3] = useUpdateProfile(auth);
+
+    const [userError,setUserError] = useState('');
+    
     const navigate = useNavigate()
-    if(loading) {
+    if(loading || loading2) {
         return <Loading></Loading>
     }
-    if(user){
+    if(user || createduser){
         navigate('/')
     }
+
+    const handleFormSubmit = async (e)=>{
+        setUserError('')
+        e.preventDefault();
+        const name = nameRef.current.value
+        const email = emailRef.current.value
+        const pass1 = pass1Ref.current.value
+        const pass2 = pass2Ref.current.value
+        if(pass1.length < 6){
+            setUserError('Password Must be 6 Charecters Long')
+            return;
+        }
+        if(pass1 !== pass2){
+            setUserError('Both password Field should be same');
+            return
+        }
+
+        await createUserWithEmailAndPassword(email,pass1)
+        await updateProfile({displayName:name});
+
+
+    }
+
     return (
         <div>
         <div className="form-container mx-2 mx-md-auto">
             <h2 className="text-main text-center mb-4">Create Account</h2>
-            <Form>
+            <Form onSubmit={handleFormSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicName">
                    
-                    <Form.Control type="text" placeholder="Enter name" required />
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter name" required />
                 
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control ref={pass1Ref} type="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword2">
-                    <Form.Control type="password" placeholder="Confirm Password" required />
+                    <Form.Control ref={pass2Ref} type="password" placeholder="Confirm Password" required />
                 </Form.Group>
+                <p><small className="text-danger">{userError}</small></p>
+                <p><small className="text-danger">{error2?.message}</small></p>
                 <p>Already have an account? <Link to="/login">Login Here</Link> </p>
                 <Button className="btn btn-outline-main bg-main text-light w-100" type="submit">
                     Create Account
