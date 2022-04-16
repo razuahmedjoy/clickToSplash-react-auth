c410af42e2f84b699c5e38550655be1f83c7566c
razuahmedjoy <rjjoybzs420@gmail.com>
2022-04-17 02:14:07 +0600

    password reset button fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../Partials/Loading/Loading';
 import SocialLogin from '../Partials/SocialLogin/SocialLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import './Login.css'
 const Login = () => {
     const navigate = useNavigate()
-    
+    const emailRef= useRef();
     const [user, loading, error] = useAuthState(auth);
     const [
         signInWithEmailAndPassword,
@@ -16,7 +18,10 @@ const Login = () => {
         loading2,
         error2,
       ] = useSignInWithEmailAndPassword(auth);
-
+    
+      const [sendPasswordResetEmail, sending,error3] = useSendPasswordResetEmail(
+        auth
+      );
     // get previous page url
     const location = useLocation()
     const from = location.state?.from?.pathname || '/home'
@@ -36,18 +41,34 @@ const Login = () => {
         signInWithEmailAndPassword(email, password)
 
     }
+
+    const handlePasswordReset = ()=>{
+        const email =  emailRef.current.value;
+        if(!email){
+            toast("Type your email and Click on Reset");
+            return;
+        }
+        else{
+            sendPasswordResetEmail(email)
+            toast('email sent');
+            
+        }
+    }
     return (
+        
         <div>
+            <ToastContainer></ToastContainer>
             <div className="form-container mx-2 mx-md-auto">
                 <h2 className="text-main text-center mb-4">Login Now</h2>
                 <p><small className="text-danger">
                     {error ? error:''}
                     {error2 ? error2?.message:''}
+                    {error3 ? error3?.message:''}
                     </small></p>
                 <Form onSubmit={handleFormSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         
-                        <Form.Control name="email" type="email" placeholder="Enter email" required />
+                        <Form.Control ref={emailRef} name="email" type="email" placeholder="Enter email" required />
                     
                     </Form.Group>
 
@@ -59,7 +80,12 @@ const Login = () => {
                     <Button className="btn btn-outline-main bg-main text-light w-100" type="submit">
                         Login
                     </Button>
+                   
+
                 </Form>
+                <p className="mt-2">
+                    <small> Forgot Your Password? 
+                        <span onClick={handlePasswordReset} className="text-main cursor-pointer" role="button">Reset</span></small> </p>
 
                 <SocialLogin></SocialLogin>
             </div>
